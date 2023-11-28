f42d1ec62de8fbe812d80530021fe74958057fcd
Michelle Schulp <michelle@marktimemedia.com>
2023-11-28 09:06:26 -0600

    style and pattern updates from testing

--- patterns/post-meta-basic.php
+++ patterns/post-meta-basic.php
@@ -8,12 +8,10 @@
  */
 
 ?>
-<!-- wp:group {"className":"post-meta post-meta-text","layout":{"type":"flex"}} -->
-<div class="wp-block-group post-meta post-meta-text">
-	<!-- wp:post-date {"isLink":true,"style":{"typography":{"fontSize":"var(\u002d\u002dwp\u002d\u002dcustom\u002d\u002dfont-sizes\u002d\u002dx-small)"}}} /-->
-	<!-- wp:paragraph -->
-	<p>by</p>
-	<!-- /wp:paragraph -->
-	<!-- wp:post-author {"showAvatar":false,"showBio":false,"style":{"typography":{"fontSize":"var(\u002d\u002dwp\u002d\u002dcustom\u002d\u002dfont-sizes\u002d\u002dx-small)"}}} /-->
-</div>
+<!-- wp:group {"style":{"spacing":{"margin":{"top":"var:preset|spacing|10","bottom":"var:preset|spacing|10"},"blockGap":"var:preset|spacing|05"},"typography":{"lineHeight":"1.2"}},"className":"post-meta post-meta-text","layout":{"type":"flex"}} -->
+<div class="wp-block-group post-meta post-meta-text" style="margin-top:var(--wp--preset--spacing--10);margin-bottom:var(--wp--preset--spacing--10);line-height:1.2"><!-- wp:post-date {"isLink":true,"style":{"typography":{"fontSize":"var(\u002d\u002dwp\u002d\u002dcustom\u002d\u002dfont-sizes\u002d\u002dx-small)"}}} /-->
+<!-- wp:paragraph -->
+<p>by</p>
+<!-- /wp:paragraph -->
+<!-- wp:post-author {"showAvatar":false,"showBio":false,"style":{"typography":{"fontSize":"var(\u002d\u002dwp\u002d\u002dcustom\u002d\u002dfont-sizes\u002d\u002dx-small)"}}} /--></div>
 <!-- /wp:group -->
--- patterns/post-meta-icons.php
+++ patterns/post-meta-icons.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Title: Post Meta With Icons
- * Slug: alicorn/three-columns-image
+ * Slug: alicorn/post-meta-icons
  * Categories: utilities
  * Description: Post author, date, categories, and tags with icons
  * Keywords: categories, tags, author, date, post meta
@@ -10,7 +10,7 @@
 ?>
 <!-- wp:group {"layout":{"inherit":true}} -->
 <div class="wp-block-group">
-    <!-- wp:group {"className":"post-meta post-meta-icons","layout":{"type":"flex"}} -->
+    <!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"className":"post-meta post-meta-icons","layout":{"type":"flex"}} -->
     <div class="wp-block-group post-meta post-meta-icons">
         <!-- wp:post-author {"showAvatar":false,"showBio":false,"style":{"typography":{"fontSize":"var(--wp--preset--font-size--x-small)"}},"className":"is-style-post-author-icon"} /-->
         <!-- wp:post-date {"isLink":true,"style":{"typography":{"fontSize":"var(--wp--preset--font-size--x-small)"}},"className":"is-style-post-date-icon"} /-->
