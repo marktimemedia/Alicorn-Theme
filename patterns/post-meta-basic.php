1050073fbcebda71a9839318d918dd573ddffb36
Michelle Schulp <michelle@marktimemedia.com>
2024-06-17 09:46:30 -0500

    remove extraneous font files, style updates

--- patterns/post-meta-basic.php
+++ patterns/post-meta-basic.php
@@ -9,7 +9,7 @@
 
 ?>
 <!-- wp:group {"style":{"spacing":{"margin":{"top":"var:preset|spacing|10","bottom":"var:preset|spacing|10"},"blockGap":"var:preset|spacing|05"},"typography":{"lineHeight":"1.2"}},"className":"post-meta post-meta-text","layout":{"type":"flex"}} -->
-<div class="wp-block-group post-meta post-meta-text" style="margin-top:var(--wp--preset--spacing--10);margin-bottom:var(--wp--preset--spacing--10);line-height:1.2"><!-- wp:post-date {"isLink":true,"style":{"typography":{"fontSize":"var(\u002d\u002dwp\u002d\u002dcustom\u002d\u002dfont-sizes\u002d\u002dx-small)"}}} /-->
+<div class="wp-block-group post-meta post-meta-text" style="margin-top:var(--wp--preset--spacing--10);margin-bottom:var(--wp--preset--spacing--10);line-height:1.2"><!-- wp:post-date {"style":{"typography":{"fontSize":"var(\u002d\u002dwp\u002d\u002dcustom\u002d\u002dfont-sizes\u002d\u002dx-small)"}}} /-->
 <!-- wp:paragraph -->
 <p>by</p>
 <!-- /wp:paragraph -->
